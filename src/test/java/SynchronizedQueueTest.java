c691b3bbeba7e45268243d75aadd0718ada80780
berkam <berkam15@mail.ru>
2020-05-31 21:30:52 +0300

    add test class

---
+++ src/test/java/SynchronizedQueueTest.java
@@ -0,0 +1,104 @@
+import org.testng.annotations.Test;
+
+public class SynchronizedQueueTest {
+
+    @Test
+    public void testAdd() {
+    }
+
+    @Test
+    public void testOffer() {
+    }
+
+    @Test
+    public void testRemove() {
+    }
+
+    @Test
+    public void testPoll() {
+    }
+
+    @Test
+    public void testElement() {
+    }
+
+    @Test
+    public void testPeek() {
+    }
+
+    @Test
+    public void testPut() {
+    }
+
+    @Test
+    public void testTestOffer() {
+    }
+
+    @Test
+    public void testTake() {
+    }
+
+    @Test
+    public void testTestPoll() {
+    }
+
+    @Test
+    public void testRemainingCapacity() {
+    }
+
+    @Test
+    public void testTestRemove() {
+    }
+
+    @Test
+    public void testContainsAll() {
+    }
+
+    @Test
+    public void testAddAll() {
+    }
+
+    @Test
+    public void testRemoveAll() {
+    }
+
+    @Test
+    public void testRetainAll() {
+    }
+
+    @Test
+    public void testClear() {
+    }
+
+    @Test
+    public void testSize() {
+    }
+
+    @Test
+    public void testIsEmpty() {
+    }
+
+    @Test
+    public void testContains() {
+    }
+
+    @Test
+    public void testIterator() {
+    }
+
+    @Test
+    public void testToArray() {
+    }
+
+    @Test
+    public void testTestToArray() {
+    }
+
+    @Test
+    public void testDrainTo() {
+    }
+
+    @Test
+    public void testTestDrainTo() {
+    }
+}
--- src/test/java/Test.java
+++
@@ -1,9 +0,0 @@
-public class Test {
-
-    private SynchronizedQueue<Integer> integers;
-
-    public void test() {
-        integers = new SynchronizedQueue<>(1);
-        integers.add(1);
-    }
-}
