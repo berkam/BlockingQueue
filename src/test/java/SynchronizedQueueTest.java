8ec8a7a15caedab1174ebeae212dbf326dd2492b
berkam <berkam15@mail.ru>
2020-06-01 19:36:52 +0300

    add tests

--- src/test/java/SynchronizedQueueTest.java
+++ src/test/java/SynchronizedQueueTest.java
@@ -1,22 +1,64 @@
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.*;
 
 public class SynchronizedQueueTest {
 
     @Test
-    public void testAdd() {
+    public void testAddPositive() {
+        BlockingQueue<Integer> synchronizedQueue = new SynchronizedQueue<>(6);
+        synchronizedQueue.addAll(Arrays.asList(1, 2, 3, 4, 5));
+        assertTrue(synchronizedQueue.add(6));
     }
 
     @Test
-    public void testOffer() {
+    public void testAddNegative() {
+        BlockingQueue<Integer> synchronizedQueue = new SynchronizedQueue<>(5);
+        synchronizedQueue.addAll(Arrays.asList(1, 2, 3, 4, 5));
+        expectThrows(IllegalStateException.class, () -> synchronizedQueue.add(6));
+    }
+
+    @Test
+    public void testOfferPositive() {
+        BlockingQueue<Integer> synchronizedQueue = new SynchronizedQueue<>(6);
+        synchronizedQueue.addAll(Arrays.asList(1, 2, 3, 4, 5));
+        assertTrue(synchronizedQueue.offer(6));
+    }
+
+    @Test
+    public void testOfferNegative() {
+        BlockingQueue<Integer> synchronizedQueue = new SynchronizedQueue<>(5);
+        synchronizedQueue.addAll(Arrays.asList(1, 2, 3, 4, 5));
+        assertFalse(synchronizedQueue.offer(6));
+    }
+
+    @Test
+    public void testOfferWithTimePositive() throws InterruptedException {
+        BlockingQueue<Integer> synchronizedQueue = new SynchronizedQueue<>(6);
+        synchronizedQueue.addAll(Arrays.asList(1, 2, 3, 4, 5));
+        assertTrue(synchronizedQueue.offer(6, 100L, TimeUnit.MILLISECONDS));
+    }
+
+    @Test
+    public void testOfferWithTimeNegative() throws InterruptedException {
+        BlockingQueue<Integer> synchronizedQueue = new SynchronizedQueue<>(5);
+        synchronizedQueue.addAll(Arrays.asList(1, 2, 3, 4, 5));
+        assertFalse(synchronizedQueue.offer(6, 100L, TimeUnit.MILLISECONDS));
     }
 
     @Test
     public void testRemove() {
+        BlockingQueue<Integer> synchronizedQueue = new SynchronizedQueue<>(5);
+        synchronizedQueue.addAll(Arrays.asList(1, 2, 3, 4, 5));
+        assertTrue(synchronizedQueue.contains(5));
+
+        synchronizedQueue.remove();
+        assertFalse(synchronizedQueue.contains(1));
     }
 
     @Test
@@ -57,6 +99,12 @@ public class SynchronizedQueueTest {
 
     @Test
     public void testTestRemove() {
+        BlockingQueue<Integer> synchronizedQueue = new SynchronizedQueue<>(5);
+        synchronizedQueue.addAll(Arrays.asList(1, 2, 3, 4, 5));
+        assertTrue(synchronizedQueue.contains(5));
+
+        synchronizedQueue.remove(5);
+        assertFalse(synchronizedQueue.contains(5));
     }
 
     @Test
@@ -77,10 +125,24 @@ public class SynchronizedQueueTest {
     }
 
     @Test
-    public void testDrainTo() {
+    public void testDrainToWithoutMaxElements() {
+        BlockingQueue<Integer> synchronizedQueue = new SynchronizedQueue<>(5);
+        ArrayList<Integer> list = new ArrayList<>();
+        synchronizedQueue.addAll(Arrays.asList(1, 2, 3, 4, 5));
+
+        assertEquals(synchronizedQueue.drainTo(list), 5);
+        assertEquals(list.size(), 5);
+        assertEquals(list, Arrays.asList(1, 2, 3, 4, 5));
     }
 
     @Test
-    public void testTestDrainTo() {
+    public void testTestDrainToWithMaxElements() {
+        BlockingQueue<Integer> synchronizedQueue = new SynchronizedQueue<>(5);
+        ArrayList<Integer> list = new ArrayList<>();
+        synchronizedQueue.addAll(Arrays.asList(1, 2, 3, 4, 5));
+
+        assertEquals(synchronizedQueue.drainTo(list, 3), 3);
+        assertEquals(list.size(), 3);
+        assertEquals(list, Arrays.asList(1, 2, 3));
     }
 }
